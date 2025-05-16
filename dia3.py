99ec92a5c810c0694bbfc581b9153bfb09bab792
ThatianaMattos <thatianamattos25@gmail.com>
2025-05-16 17:16:27 -0300

    fix: ajusta expressão lógica na verificação de acesso do usuário

--- dia3.py
+++ dia3.py
@@ -90,8 +90,7 @@ senha_inserida = "abcd1234"
 senha_correta = "abcd1234"
 usuario_bloqueado = False
 # Verificar acesso
-acesso_concedido = (senha_inserida == senha_correta) and not
-usuario_bloqueado
+acesso_concedido = (senha_inserida == senha_correta) and not usuario_bloqueado
 print("Acesso concedido?", acesso_concedido) # True
 
 
