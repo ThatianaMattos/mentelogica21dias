1708fd06f4e2040a4af827963bccc26f820e0647
ThatianaMattos <thatianamattos25@gmail.com>
2025-05-16 17:31:32 -0300

    fix: ajusta concatenação de string com conversão de tipo usando f-string no exercício de variáveis

--- dia2.py
+++ dia2.py
@@ -1,5 +1,107 @@
 #variáveis
 
+
+idade = 25
+nome = "Maria"
+altura = 1.68
+
+
+idade = 30
+quantidade = -5
+ano = 2021
+
+
+
+altura = 1.75
+peso = 68.5
+temperatura = -3.4
+
+
+
+nome = "João"
+cidade = 'São Paulo'
+mensagem = "Olá, mundo!"
+
+
+ligado = True
+maior_de_idade = False
+
+
+idade = 28 # inteiro
+altura = 1.70 # float
+nome = "Ana" # string
+estudante = True # booleano
+
+
+print(type(idade)) # <class 'int'>
+print(type(altura)) # <class 'float'>
+print(type(nome)) # <class 'str'>
+print(type(estudante)) # <class 'bool'>
+
+
+soma = 5 + 3 # 8
+produto = 2 * 4.5 # 9.0
+
+saudacao = "Olá, " + "Maria!" # "Olá, Maria!"
+
+
+maior = 10 > 5 # True
+igual = 4 == 4 # True
+diferente = 3 != 2 # True
+
+
+nome = "Carlos"
+idade = 22
+altura = 1.80
+estudante = True
+
+
+print("Nome:", nome)
+print("Idade:", idade)
+print("Altura:", altura)
+print("Estudante:", estudante)
+
+
+ano_nascimento = 2023 - idade
+print("Ano de Nascimento:", ano_nascimento)
+
+maior_de_idade = idade >= 18
+
+print("Maior de idade:", maior_de_idade)
+
+
+
+frase = "Olá, meu nome é " + nome + " e eu tenho " + str(idade) + " anos."
+print(frase)
+
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+soma = numero1 + numero2
+subtracao = numero1 - numero2
+multiplicacao = numero1 * numero2
+divisao = numero1 / numero2
+print("Soma:", soma)
+print("Subtração:", subtracao)
+print("Multiplicação:", multiplicacao)
+print("Divisão:", divisao)
+
+
+
+
+celsius = float(input("Digite a temperatura em Celsius: "))
+fahrenheit = celsius * 9/5 + 32
+print("A temperatura em Fahrenheit é:", fahrenheit)
+
+
+raio = float(input("Digite o raio do círculo: "))
+pi = 3.14159
+area = pi * raio ** 2
+print("A área do círculo é:", area)
+
+
+
+
+
 #variável numérica
 
 idade = 42
