71e96a47c09df4b867c6b151abcc713c335011e3
ThatianaMattos <thatianamattos25@gmail.com>
2025-06-16 17:28:17 -0300

    "feat: adicionar exercícios de variáveis e concatenação em Python"

---
+++ exedia1.py
@@ -0,0 +1,3 @@
+print('Oi meu nome é Thatiana ')
+print("Eu quero a minha independência financeira")
+print("Não aguento mais essa vida de dona de casa")
---
+++ exedia2.py
@@ -0,0 +1,22 @@
+idade = 43
+nome = "Thatiana"
+altura = 1.41
+estudante = True
+
+print("Nome:", nome)
+print("Idade:", idade)
+print("Altura:", altura)
+print("Estudante:", estudante)
+
+ano_nascimento = 2025 - idade
+print("Ano de Nascimento:", ano_nascimento)
+maior_de_idade = idade>=18
+print("Maior de idade:", maior_de_idade)
+
+frase = "Olá, meu nome é " + nome + " e eu tenho " + str(idade) + " anos." 
+print(frase)
+
+
+
+
+
