de3037aa70f251cf47575875b429af44a2cfeafa
ThatianaMattos <thatianamattos25@gmail.com>
2025-06-16 18:07:34 -0300

    feat: adicionar operações matemáticas, conversão de temperatura e cálculo de área do círculo no exedia2.py

--- exedia2.py
+++ exedia2.py
@@ -16,6 +16,28 @@ print("Maior de idade:", maior_de_idade)
 frase = "Olá, meu nome é " + nome + " e eu tenho " + str(idade) + " anos." 
 print(frase)
 
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+
+soma = numero1 + numero2
+subtracao = numero1 - numero2
+multiplicacao = numero1 * numero2
+divisao = numero1 / numero2
+
+print("Soma:", soma)
+print("Subtração:", subtracao)
+print("Multiplicação:", multiplicacao)
+print("Divisão:", divisao)
+
+celsius = float(input("Digite a temperatura em Celsius: "))
+fahrenheit = celsius * 9/5 + 32
+print("A temperatura em Fahrenheit é:", fahrenheit)
+
+raio = float(input("Digite o raio do círculo: "))
+pi = 3.14159
+area = pi * raio ** 2
+print("A área do círculo é:", area)
+
 
 
 
